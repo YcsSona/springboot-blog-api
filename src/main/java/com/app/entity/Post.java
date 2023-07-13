9d6356fa98aa07ce605da93af3049ab83a94d8e3
ycssona <ycssona@gmail.com>
2023-07-13 16:57:43 +0530

    entity: Added Post entity

---
+++ src/main/java/com/app/entity/Post.java
@@ -0,0 +1,26 @@
+package com.app.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post extends BaseEntity {
+
+	@Column(unique = true, nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column(nullable = false)
+	private String content;
+}
