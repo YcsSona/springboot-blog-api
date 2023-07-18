f313cfafdbe0dac655de8a0e14b2b6c18a49a4da
ycssona <ycssona@gmail.com>
2023-07-18 07:57:37 +0530

    entity: Post: Added matching constructor to avoid errors

--- src/main/java/com/app/entity/Post.java
+++ src/main/java/com/app/entity/Post.java
@@ -20,6 +20,13 @@ import lombok.NoArgsConstructor;
 @Table(name = "posts")
 public class Post extends BaseEntity {
 
+	public Post(String title, String description, String content) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.content = content;
+	}
+
 	@Column(unique = true, nullable = false)
 	private String title;
 
