0abbdd23dd6d3857ad5bab9671a08ba87a0764a1
ycssona <ycssona@gmail.com>
2023-07-18 08:05:58 +0530

    dto: Added comment dto

---
+++ src/main/java/com/app/payload/CommentDto.java
@@ -0,0 +1,19 @@
+package com.app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentDto {
+
+	private Long id;
+
+	private String name;
+
+	private String email;
+
+	private String body;
+}
