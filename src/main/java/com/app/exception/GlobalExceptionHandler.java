399a23d00534223ca3966c7462b066895e48eb1e
ycssona <ycssona@gmail.com>
2023-07-19 23:41:15 +0530

    GlobalHandlerException: MethodArgumentNotValidException: Customizing validation response

--- src/main/java/com/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/exception/GlobalExceptionHandler.java
@@ -1,15 +1,35 @@
 package com.app.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.app.payload.ErrorDetails;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		Map<String, String> errors = new HashMap<>();
+
+		ex.getBindingResult().getAllErrors().forEach(e -> {
+			errors.put(((FieldError) e).getField(), e.getDefaultMessage());
+		});
+
+		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+	}
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<?> handleResponseNotFoundException(ResourceNotFoundException ex, WebRequest request) {
