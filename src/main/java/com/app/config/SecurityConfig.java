4f0eb87903971727093870e4c17c356f87e8b108
ycssona <ycssona@gmail.com>
2023-07-21 16:50:13 +0530

    security: Added support of Database authentication

--- src/main/java/com/app/config/SecurityConfig.java
+++ src/main/java/com/app/config/SecurityConfig.java
@@ -1,18 +1,18 @@
 package com.app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,21 +20,29 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//		UserDetails theonewhocode = User.builder()
+//				.username("theonewhocode")
+//				.password(passwordEncoder().encode("theonewhocode"))
+//				.roles("USER")
+//				.build();
+//		
+//		UserDetails admin= User.builder()
+//				.username("admin")
+//				.password(passwordEncoder().encode("admin"))
+//				.roles("ADMIN")
+//				.build();
+//		
+//		return new InMemoryUserDetailsManager(theonewhocode, admin);
+//	}
+	
 	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails theonewhocode = User.builder()
-				.username("theonewhocode")
-				.password(passwordEncoder().encode("theonewhocode"))
-				.roles("USER")
-				.build();
-		
-		UserDetails admin= User.builder()
-				.username("admin")
-				.password(passwordEncoder().encode("admin"))
-				.roles("ADMIN")
-				.build();
-		
-		return new InMemoryUserDetailsManager(theonewhocode, admin);
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
 	}
 	
 	@Bean
---
+++ src/main/java/com/app/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.app.security;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.app.entity.User;
+import com.app.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+		User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(
+				() -> new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail));
+
+		Set<GrantedAuthority> authorities = user.getRoles().stream()
+				.collect(Collectors.mapping(role -> new SimpleGrantedAuthority(role.getName()), Collectors.toSet()));
+		
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+	}
+
+}
