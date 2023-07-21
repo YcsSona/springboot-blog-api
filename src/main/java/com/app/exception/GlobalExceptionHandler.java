87451457ec30ca0bfcb9e351607c8176438b8c12
ycssona <ycssona@gmail.com>
2023-07-21 11:18:06 +0530

    security: Handling AccessDeniedException to return UNAUTHORIZED(401) instead of INTERNAL_SERVER_ERROR(500)

--- src/main/java/com/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -42,6 +43,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<?> handleBlogAPIException(BlogAPIException ex, WebRequest request) {
 		ErrorDetails details = new ErrorDetails(ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException ex, WebRequest request) {
+		ErrorDetails details = new ErrorDetails(ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(details, HttpStatus.UNAUTHORIZED);
 	}
 
 	@ExceptionHandler(RuntimeException.class)
